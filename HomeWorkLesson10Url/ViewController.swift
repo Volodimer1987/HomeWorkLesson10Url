d85cb182cd14456a97c43d6c039cb760d4f520ed
Vladimir <kynicin87@mail.ru>
2020-10-12 12:47:36 +0300

    Initial Commit

---
+++ HomeWorkLesson10Url/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkLesson10Url
+//
+//  Created by vladimir gennadievich on 12.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
